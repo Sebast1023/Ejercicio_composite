6c26ea2e1448527a30b20ab470be8c9eec3f048f
YMB030 <129144487+YMB030@users.noreply.github.com>
2026-04-14 00:27:26 -0500

    imp resta

--- ejemplo.py
+++ ejemplo.py
@@ -2,11 +2,12 @@ from numero import Numero
 from suma import Suma
 from multiplicacion import Multiplicacion
 from division import Division
+from resta import Resta
 
-#((4+5)+(3*2))/3
-ejemplo = Division(Suma(Suma(Numero(4),Numero(5)), Multiplicacion(Numero(3),Numero(2))), Numero(3))
+#((4+5)+(3*2)-6)/3
+ejemplo = Division(Resta(Suma(Suma(Numero(4),Numero(5)), Multiplicacion(Numero(3),Numero(2))),Numero(6)), Numero(3))
 
 try:
-    print("((4+5)+(3*2))/3 = "+ str(ejemplo.evaluar()))
+    print("((4+5)+(3*2)-6)/3 = "+ str(ejemplo.evaluar()))
 except ValueError as e:
     print("Error en la expresión:", e)
